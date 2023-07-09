8115bbf74d994db5c06815d1ab49cbbda5d3f427
Sergei <sgorcev84@mail.ru>
2023-07-09 23:01:52 +0300

    [+] Генерация с помощью cmake заголовочного файла с версией проекта, указанной в project.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "version.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "build N" << endl;
+    cout << "build " << PROJECT_VERSION << endl;
     cout << "Hello, World!" << endl;
     return 0;
 }
